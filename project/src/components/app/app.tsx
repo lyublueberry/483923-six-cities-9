a0c646a456cb56207c44b772e3774cd406733d3f
lyublueberry <lara.87@bk.ru>
2022-02-20 19:49:00 +0400

    Заменит Route на PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import NotFoundPageScreen from '../page-not-found/page-not-found';
 import LoginScreen from '../login-screen/login-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import PropertyScreen from '../property-screen/property-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScrenProps = {
   placesCount: number;
@@ -14,11 +15,19 @@ function App({ placesCount }: AppScrenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Root} element={<MainScreen placesCount={placesCount}/>}/>
-        <Route path={AppRoute.Login} element={<LoginScreen/>}/>
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen/>}/>
-        <Route path={AppRoute.Offer} element={<PropertyScreen/>}/>
-        <Route path="*" element={<NotFoundPageScreen/>}/>
+        <Route path={AppRoute.Root} element={<MainScreen placesCount={placesCount} />} />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Offer} element={<PropertyScreen />} />
+        <Route path="*" element={<NotFoundPageScreen />} />
       </Routes>
     </BrowserRouter>
   );
