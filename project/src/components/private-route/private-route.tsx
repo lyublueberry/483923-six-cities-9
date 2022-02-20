9271278095172e2a2567d0c6e9cadf121b829c82
lyublueberry <lara.87@bk.ru>
2022-02-20 19:35:11 +0400

    добавит компонент приватного маршрута

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,17 @@
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { Navigate } from 'react-router-dom';//компонент для перенаправления, когда не нужна логика
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteProps):JSX.Element {
+  const{authorizationStatus, children} = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth ? children : <Navigate to={AppRoute.Login}/>
+  );
+}
+
+export default PrivateRoute;
--- project/src/const.ts
+++ project/src/const.ts
@@ -5,3 +5,9 @@ export enum AppRoute {
   Favorites = '/favorites',
   Offer = '/offer:id'
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
+}
