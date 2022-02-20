460547201ffaec76f2ae72488b5687217dbdd4c7
lyublueberry <lara.87@bk.ru>
2022-02-20 19:23:22 +0400

    Обновит ссылку для логотипа

--- project/src/components/page-not-found/page-not-found.tsx
+++ project/src/components/page-not-found/page-not-found.tsx
@@ -1,6 +1,9 @@
+import Logo from '../logo/logo';
+
 function NotFoundPageScreen(): JSX.Element {
   return (
     <div className="notpage">
+      <Logo/>
       <img alt="4453 404 Page Asset" className="lozad" data-src="https://www-cdn.bigcommerce.com/assets/4453_404Page_Asset.png?mtime=20190313003628" title="4453 404 Page Asset" height="662" width="548" src="https://www-cdn.bigcommerce.com/assets/4453_404Page_Asset.png?mtime=20190313003628" data-loaded="true" />
     </div>
   );
