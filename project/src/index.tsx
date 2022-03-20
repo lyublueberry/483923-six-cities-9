627c6e2024dd0306169d42820853f3165dc7ef51
lyublueberry <lara.87@bk.ru>
2022-03-20 18:23:41 +0400

    Подключит redux к проекту

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
-import {offers} from './mocks/offers';
+import { offers } from './mocks/offers';
+import { store } from './store';
 
 const Settings = {
   PLACE_COUNT: 5,
@@ -9,6 +11,9 @@ const Settings = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App placesCount={Settings.PLACE_COUNT} offers = {offers}/>
+    <Provider store={store}>
+      <App placesCount={Settings.PLACE_COUNT} offers={offers} />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root'));
