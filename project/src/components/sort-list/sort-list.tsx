8a27d7217def0707cdeb4a5fab2211b90653e5f6
Lara87 <lara.87@bk.ru>
2022-05-09 17:06:59 +0400

    добавление закрыто/открыто селекта выбора для сортировки

--- project/src/components/sort-list/sort-list.tsx
+++ project/src/components/sort-list/sort-list.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { SortType } from '../../const';
 import { useAppSelector } from '../../hooks';
 import SortItem from '../sort-item/sort-item';
 
 function SortList(): JSX.Element {
+  const [isOpenOptions, setIsOpenOptions] = useState(false);
+
   const sortType = useAppSelector((state) => state.sortType);
   return (
-    <form className="places__sorting" action="#" method="get">
+    <form onClick={(evt) => { evt.preventDefault(); return setIsOpenOptions(!isOpenOptions); }} className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by </span>
       <span className="places__sorting-type" tabIndex={0}>
         {sortType}
@@ -13,13 +16,15 @@ function SortList(): JSX.Element {
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        {
-          Object.values(SortType).map((sortIt) => (
-            <SortItem key={sortIt} sortType={sortIt} />
-          ))
-        }
-      </ul>
+      {isOpenOptions &&
+        <ul className="places__options places__options--custom places__options--opened">
+          {
+            Object.values(SortType).map((sortIt) => (
+              <SortItem key={sortIt} sortType={sortIt} />
+            ))
+          }
+        </ul>}
+
     </form>
   );
 }
